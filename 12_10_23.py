3e02fbe23ac68ddea3ebdaea3c82e6ba470cb66f
Ranjit-Singh-786 <jiradhey402@gmail.com>
2023-10-12 11:16:14 +0530

    12 class added

---
+++ 12_10_23.py
@@ -0,0 +1,60 @@
+# tpl = (2,5,8,6,7)
+# print(max(tpl))
+# print(min(tpl))
+# print(sum(tpl))
+# tpl = ('upfliars','jaipur','rajsthan')
+# a , b ,c = tpl
+# print(tpl)
+# print(a)
+# print(b)
+# print(c)
+# print(tpl.count('upfliars'))
+# print(tpl.index('jaipur'))
+
+
+
+# s = {20,'jessa',12.5,True,(2,5,8,7),{2,5,4,7}}
+# print(type(s))
+# print(s)
+# print(s[0])
+# s[0] = 25
+# print(s)
+# s  = {2,2,2,2,2,5}
+# print(s)
+
+# a = '25a'
+# int(),float(),str()
+# print(type(str(a)))
+# print(str(a))
+# print(int(a))
+
+# lst = [2,5,4,8,6,4,8,1,2,4,2,5,3,2]
+# print(lst)
+# print(set(lst))
+
+# s = {2,4,5,8,6}
+# s2 = {2,4,5,8,6}
+# s.add(522)
+# print(s2)
+
+# s2.remove(2)
+# s2.discard(5)
+
+# s2 = {2,4,5,25,45,'a','c',8,6}
+# print(s2)
+# s2.pop()
+# print(s2)
+# s2 = {2,4,5,25,45,'a','c',8,6}
+# s3 = {10,200,300,45,'a'}
+# s2.update(s3)
+# s2.clear()
+# print(s2)
+
+# dc = {'a':10,'b':20,'c':30}
+# print(dc,'\n')
+# print()
+# print(type(dc))
+# dc = {'a':10,'b':20.5,'c':True,'d':'upflairs','e':[25,7,5],'f':(5,4,58),'g':{8,5,4,6}}
+
+# dc = {'a':10,20:20.5,8.5:52,True:'upflairs'}
+# print(dc)
