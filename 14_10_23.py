3faf05cd2028de5f356d41fab44661d1e1262b7e
Ranjit-Singh-786 <jiradhey402@gmail.com>
2023-10-14 14:51:31 +0530

    14th class added

---
+++ 14_10_23.py
@@ -0,0 +1,110 @@
+# d = {'a':10,'b':20,'c':30,'d':40,'e':50}
+# print(d['a'])
+# print(d.keys())
+# print(d.values())
+# d['a'] = 100
+# print(d['a'])
+# d.pop('a')
+# print(d)
+d = {'a':10,'b':20,'c':30,'d':40,'e':50}
+e = {'f':60,'g':70}
+
+# d.update(e)
+# print(d['ab'])
+# print(d.get('ab','Not found'))
+# print(d)
+# a = 10
+# b = 5
+
+# print(a > b)
+
+
+# conditional statement
+
+# a = 10
+# b = 5
+# if a > b:
+#     print('it is True')
+# else:
+#     print('it is false')
+
+# a = 10
+# b = 5
+# if a > b:
+#     print('it is True')
+# elif a > b:
+#     print('it is also true')
+# else:
+#     print('it is false')
+
+# a = 10
+# b = 5
+# if a < b:
+#     if a == b:
+#         print('equal')
+#     else:
+#         print('thi is not equal')
+# else:
+#     print('a is less than')
+
+# num = int(input('eneter your number :-'))
+
+# if num % 2 == 0:
+#     print('even')
+# else :
+#     print('odd')
+
+# num = int(input('eneter your number :-'))
+
+# print( num+2 )
+# num += 2
+# num = num + 2
+# # num + 2
+# print(num)
+
+
+# a = 20
+# b = 10
+# if (a > b) and (a == b):
+#     print('it is true')
+# else:
+#     print('it is not tue')
+
+
+# a = 20
+# b = 10
+# if (a < b) or (a == b):
+#     print('it is true')
+# else:
+#     print('it is not tue')
+
+
+
+# a = False
+# print(a)
+# print(not(a))   
+
+# a = [2,5,6,4,8]
+
+# if 2 not in a:
+#     print("true")
+# else:
+#     print('False')
+
+
+# a = 10
+# b = 5
+# temp = "true" if a < b else "False"  "hmm its true" if a > b else 'it is false'
+# print(temp)
+
+# print('upflairs')
+# print('upflairs')
+# print('upflairs')
+# print('upflairs')
+# print('upflairs')
+# print('upflairs')
+lst = [2,5,56,7,4,6,94,8,7,22,11,4]
+# for i in range(len(lst)):
+#     # print(i)
+#     if lst[i] == 8:
+#         print(i)
