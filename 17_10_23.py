57702c561c55744564c6e77feb719dfe35a829a2
Ranjit-Singh-786 <jiradhey402@gmail.com>
2023-10-17 15:57:56 +0530

    17-10-23 class added

---
+++ 17_10_23.py
@@ -0,0 +1,29 @@
+# for i in range(10):
+#     print(i,' hello world')
+
+# name = 'upflairs jaipur'
+# for i in name:
+#     if i =="j":
+#         print('exist')
+
+# lst = [2,5,4,7,8,9,6]
+# for i in lst:
+#     print(i)
+# tpl = (2,5,4,7,8,9,60)
+# for i in tpl:
+#     print(i)
+
+# d = {'a':10,'b':20,'c':30,'d':40}
+# for key,value in d.items():
+#     print(key,value)
+
+name = 'upflairs jaipur'
+dic = {}
+for char in name:
+    dic[char] = 
+   
+
+
+
+
+
