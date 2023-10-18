a26141f0aececea8855a70ec19b56dafbd5a9eb0
Ranjit-Singh-786 <jiradhey402@gmail.com>
2023-10-18 16:31:24 +0530

    18 class added

---
+++ 18_10_23.py
@@ -0,0 +1,24 @@
+# num = 2
+# i = 1
+# while i <=10:
+#     print(num * i)
+#     i+=1
+
+# print even number using while loop in a range
+
+# i = 0
+# num = 0
+# while i<=10:
+#     if i %2 == 0:
+#         print(i)
+#     i+=1
+
+
+# 1,2,3,
+# 1+2+3+4+5+6+7+8+9+10
+# temp =0
+# i =0
+# while i<=10:
+#     temp = temp + i
+#     i+=1
+# print(temp)
