12f165cce02a442fed2d555f4e77176d1296277b
Ranjit-Singh-786 <jiradhey402@gmail.com>
2023-10-27 13:39:35 +0530

    27 class added

---
+++ 27_10_23.py
@@ -0,0 +1,66 @@
+# # print('program execution start')
+# # print('program processing')
+# # try:
+# #     num = int(input('enter your first number '))
+# #     num2 = int(input('enter your second number '))
+# #     result = num / num2
+# #     print(result)
+# # except Exception as e:
+# #     print('zero devision by  error')
+# #     print(e)
+# # else:
+# #     print('exception is not occured')
+# # finally:
+# #     print('me to chalunga')
+
+# # print('program execution finished')
+
+
+
+# try:
+#     num = int(input('enter your number :- '))
+#     print(num)
+#         print('dhfkdhfk')
+# except ValueError:
+#     print('bhai error  a gyi ')
+
+# except IndentationError:
+#     print('syntex thik kro')
+
+
+
+# # int(input('enter number'))2
+
+
+
+# temp = []
+# for i in range(1,11):
+#     temp.append(i**2)
+# print(temp)
+# print()
+
+# lst_of_square = [i**2 for i in range(1,11)]
+# print(lst_of_square)
+
+
+# lst = [i for i in range(1,21)]
+# print(lst)
+
+# evens = []
+# for i in range(1,50):
+#     if i%2==0:
+#         evens.append(i)
+# print(evens)
+
+# lst = ['even' if i %2==0 else 'odd' for i in range(1,50) ]
+# print(lst)
+
+# lst = [[i for i in range(1,)] for i in range(1,10)]
+# print(lst)
+
+
+# lst = list(range(1,21))
+# def even(x):
+#     return x%2==0
+# result = list(map(even , lst))
+# print(result)
