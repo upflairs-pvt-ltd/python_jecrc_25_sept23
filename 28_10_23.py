4cf2ac55c64f468399be41ce0463ebddf618d8d7
Ranjit-Singh-786 <jiradhey402@gmail.com>
2023-10-28 12:15:03 +0530

    28 class added

---
+++ 28_10_23.py
@@ -0,0 +1,30 @@
+# import test
+
+# num1 = int(input('enter your first number '))
+# num2 = int(input('enter your second number '))
+
+
+# result= test.sum(num1 , num2)
+# print(result)
+
+
+# from test import sum ,multiply
+# from test import multiply
+# from test import *
+
+# num1 = int(input('enter your first number '))
+# num2 = int(input('enter your second number '))
+
+# result= sum(num1 , num2)
+# print(result)
+# print()
+# result2 = multiply(num1,num2)
+# print(result2)
+
+
+
+# OS MODULE
+import os
+# print(os.getcwd())
+
+os.listdir()
---
+++ test.py
@@ -0,0 +1,9 @@
+def sum(a ,b):
+    return a +b
+
+print('hello test.py  file')
+
+def multiply(a,b):
+    return a * b
+
+help('modules')
